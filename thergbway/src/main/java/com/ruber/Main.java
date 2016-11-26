55249ef91ccc0ee12b9dd18fe72236abaa4135f7
Andrey Selivanov <thergbway@yandex-team.ru>
2016-11-26 13:36:18 +0300

    added hello world application

---
+++ thergbway/src/main/java/com/ruber/Main.java
@@ -0,0 +1,10 @@
+package com.ruber;
+
+/**
+ * Created by thergbway on 26.11.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
